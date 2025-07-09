29078187da178bdada833bfbbf01705e8e3e5dd8
Neirn <neirny@gmail.com>
2025-07-08 23:34:30 -0700

    stuff

--- src/extlib/lib_main.cpp
+++ src/extlib/lib_main.cpp
@@ -1,14 +1,384 @@
 #include <iostream>
-
+#include <filesystem>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <array>
+#include <algorithm>
 #include "lib_recomp.hpp"
+#include "defines_z64o.h"
+#include "defines_ooto.h"
+#include "defines_mmo.h"
+
+// Keep in sync with playermodelmanager_api.h
+#define PMM_MAX_INTERNAL_NAME_LENGTH 64
+#define PMM_MAX_AUTHOR_NAME_LENGTH 64
+#define PMM_MAX_DISPLAY_NAME_LENGTH 32
+
+namespace fs = std::filesystem;
+
+struct ModelDiskEntry {
+    std::string internalName;
+    std::string displayName;
+    std::string authorName;
+    size_t fileSize;
+    char *modelData;
+};
+
+static std::vector<ModelDiskEntry> sModelDiskEntries;
 
 extern "C" {
     DLLEXPORT uint32_t recomp_api_version = 1;
 }
 
-RECOMP_DLL_FUNC(native_lib_test) {
-    std::string mod_text = RECOMP_ARG_STR(0);
+// Check if zobj is standalone.
+// Moodifies stream input position.
+bool isValidStandaloneZobj(std::ifstream& f) {
+    if (!f) {
+        return false;
+    }
 
-    std::cout << mod_text << "\n";
-    RECOMP_RETURN(int, 0);
+    {
+        const size_t MINIMUM_STANDALONE_ZOBJ_SIZE = 0x5800;
+
+        f.seekg(MINIMUM_STANDALONE_ZOBJ_SIZE);
+        
+        char tmp;
+        f.read(&tmp, 1);
+
+        if (f.eof()) {
+            return false;
+        }
+    }
+
+    f.seekg(Z64O_MODLOADER_HEADER);
+
+    const std::array<char, sizeof("MODLOADER64")> MODLOADER64_HEADER("MODLOADER64");
+    std::array<char, MODLOADER64_HEADER.size()> headerBuf;
+
+    f.read(headerBuf.data(), headerBuf.size());
+
+    if (f.eof()) {
+        return false;
+    }
+
+    if (memcmp(MODLOADER64_HEADER.data(), headerBuf.data(), MODLOADER64_HEADER.size() - 1) == 0) {
+        switch (headerBuf[headerBuf.size() - 1]) {
+            case MMO_FORM_BYTE_CHILD:
+            case MMO_FORM_BYTE_ADULT:
+            case OOTO_FORM_BYTE_ADULT:
+            case OOTO_FORM_BYTE_CHILD:
+                return true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    return false;
+}
+
+void extractEmbeddedInfo(const std::vector<char>& v, ModelDiskEntry& entry) {
+    const std::array<char, sizeof("PLAYERMODELINFO")> PLAYER_MODEL_INFO_HEADER_STR("PLAYERMODELINFO");
+
+    const size_t EMBEDDED_INFO_HEADER_LOCATION = 0x5500;
+
+    if (memcmp(v.data() + EMBEDDED_INFO_HEADER_LOCATION, PLAYER_MODEL_INFO_HEADER_STR.data(), PLAYER_MODEL_INFO_HEADER_STR.size() - 1) == 0) {
+        const size_t EMBEDDED_INFO_VERSION_LOCATION = EMBEDDED_INFO_HEADER_LOCATION + PLAYER_MODEL_INFO_HEADER_STR.size() - 1;
+
+        if (v[EMBEDDED_INFO_VERSION_LOCATION] == 1) {
+            const size_t INTERNAL_NAME_FIELD_SIZE = 64;
+            const size_t DISPLAY_NAME_FIELD_SIZE = 32;
+            const size_t AUTHOR_NAME_FIELD_SIZE = 64;
+
+            const size_t EMBEDDED_INTERNAL_NAME_LOCATION = EMBEDDED_INFO_HEADER_LOCATION + PLAYER_MODEL_INFO_HEADER_STR.size();
+            const size_t EMBEDDED_DISPLAY_NAME_LOCATION = EMBEDDED_INTERNAL_NAME_LOCATION + INTERNAL_NAME_FIELD_SIZE;
+            const size_t EMBEDDED_AUTHOR_NAME_LOCATION = EMBEDDED_DISPLAY_NAME_LOCATION + DISPLAY_NAME_FIELD_SIZE;
+
+            auto setEntryStr = [v](size_t offset, size_t maxSize, std::string& result) {
+                auto start = v.begin() + offset;
+                auto end = start + maxSize;
+
+                auto it = start;
+                while (*it != '\0' && it != end) {
+                    it++;
+                }
+
+                // Don't read null terminator into string
+                if (it > start && *it == '\0') {
+                    it--;
+                }
+
+                result = start != it ? std::string(start, it) : "";
+            };
+
+            setEntryStr(EMBEDDED_INTERNAL_NAME_LOCATION, INTERNAL_NAME_FIELD_SIZE, entry.internalName);
+            setEntryStr(EMBEDDED_DISPLAY_NAME_LOCATION, DISPLAY_NAME_FIELD_SIZE, entry.displayName);
+            setEntryStr(EMBEDDED_AUTHOR_NAME_LOCATION, AUTHOR_NAME_FIELD_SIZE, entry.authorName);
+        }
+    }
+}
+
+RECOMP_DLL_FUNC(PMMZobj_scanForDiskEntries) {
+    sModelDiskEntries.clear();
+
+    std::string folderPath = RECOMP_ARG_STR(0);
+
+    fs::directory_entry folderEntry(folderPath);
+
+    if (!folderEntry.is_directory()) {
+        RECOMP_RETURN(int, -1);
+    }
+
+    // Don't bother evaluating files >= 4 MiB
+    // If a model is that big, it should be replaced through RT64 instead
+    const size_t MAX_MODEL_SIZE = 4 * 1024 * 1024;
+
+    for (const auto &dirEntry : fs::directory_iterator(folderEntry.path())) {
+        if (dirEntry.file_size() < MAX_MODEL_SIZE) {
+            if (dirEntry.is_regular_file() || (dirEntry.is_symlink() && fs::is_regular_file(fs::weakly_canonical(dirEntry.path())))) {
+                std::ifstream file(dirEntry.path(), std::ios::in | std::ios::binary);
+
+                if (isValidStandaloneZobj(file)) {
+                    file.seekg(0);
+                    
+                    std::vector<char> fileBuf(std::istreambuf_iterator(file), {});
+
+                    ModelDiskEntry mde;
+
+                    extractEmbeddedInfo(fileBuf, mde);
+
+                    mde.fileSize = fileBuf.size();
+
+                    fs::path dirEntryPath = dirEntry.path();
+
+                    if (mde.internalName == "") {
+                        mde.internalName = dirEntryPath.filename().string();
+
+                        if (mde.internalName.size() > PMM_MAX_INTERNAL_NAME_LENGTH) {
+                            mde.internalName = mde.internalName.substr(0, PMM_MAX_AUTHOR_NAME_LENGTH);
+                        }
+                    }
+
+                    if (mde.displayName == "") {
+                        mde.displayName = dirEntryPath.stem().string();
+
+                        if (mde.displayName.size() > PMM_MAX_INTERNAL_NAME_LENGTH) {
+                            mde.displayName = mde.displayName.substr(0, PMM_MAX_DISPLAY_NAME_LENGTH);
+                        }
+                    }
+
+                    if (mde.authorName == "") {
+                        mde.authorName = "N/A";
+                    }
+
+                    mde.modelData = new char[fileBuf.size()];
+
+                    memcpy(mde.modelData, fileBuf.data(), fileBuf.size());
+
+                    sModelDiskEntries.push_back(mde);
+                }
+            }
+        }
+    }
+
+    RECOMP_RETURN(int, sModelDiskEntries.size());
+}
+
+RECOMP_DLL_FUNC(PMMZobj_getNumDiskEntries) {
+    RECOMP_RETURN(int, sModelDiskEntries.size());
+}
+
+ModelDiskEntry *getDiskEntry(int i) {
+    return i >= sModelDiskEntries.size() || i < 0 ? nullptr : &sModelDiskEntries[i];
+}
+
+bool writeEntryDataToRecompBuffer(uint8_t *rdram, recomp_context *ctx, const void *data, const size_t dataSize) {
+    int bufLen = RECOMP_ARG(int, 2);
+
+    if (bufLen < dataSize) {
+        return false;
+    }
+
+    PTR(char)
+    buf = RECOMP_ARG(PTR(char), 1);
+
+    const char *bytes = static_cast<const char*>(data);
+
+    for (size_t i = 0; i < dataSize; i++) {
+        MEM_B(buf, i) = bytes[i];
+    }
+}
+
+bool writeEntryStringToRecompBuffer(uint8_t *rdram, recomp_context *ctx, const std::string& s) {
+    return writeEntryDataToRecompBuffer(rdram, ctx, s.c_str(), s.size() + 1);
+}
+
+RECOMP_DLL_FUNC(PMMZobj_entryInternalNameLength) {
+    int index = RECOMP_ARG(int, 0);
+
+    ModelDiskEntry *entry = getDiskEntry(index);
+
+    if (!entry) {
+        RECOMP_RETURN(int, -1);
+    }
+
+    RECOMP_RETURN(int, entry->internalName.size());
+}
+
+RECOMP_DLL_FUNC(PMMZobj_entryDisplayNameLength) {
+    int index = RECOMP_ARG(int, 0);
+
+    ModelDiskEntry *entry = getDiskEntry(index);
+
+    if (!entry) {
+        RECOMP_RETURN(int, -1);
+    }
+
+    RECOMP_RETURN(int, entry->displayName.size());
+}
+
+RECOMP_DLL_FUNC(PMMZobj_entryAuthorNameLength) {
+    int index = RECOMP_ARG(int, 0);
+
+    ModelDiskEntry *entry = getDiskEntry(index);
+
+    if (!entry) {
+        RECOMP_RETURN(int, -1);
+    }
+
+    RECOMP_RETURN(int, entry->authorName.size());
+}
+
+RECOMP_DLL_FUNC(PMMZobj_writeInternalNameToBuffer) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    RECOMP_RETURN(bool, writeEntryStringToRecompBuffer(rdram, ctx, entry->internalName));
+}
+
+RECOMP_DLL_FUNC(PMMZobj_writeDisplayNameToBuffer) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    RECOMP_RETURN(bool, writeEntryStringToRecompBuffer(rdram, ctx, entry->displayName));
+}
+
+RECOMP_DLL_FUNC(PMMZobj_writeAuthorNameToBuffer) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    RECOMP_RETURN(bool, writeEntryStringToRecompBuffer(rdram, ctx, entry->authorName));
+}
+
+RECOMP_DLL_FUNC(PMMZobj_getEntryFileSize) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(int, -1);
+    }
+
+    RECOMP_RETURN(int, entry->fileSize);
+}
+
+RECOMP_DLL_FUNC(PMMZobj_getEntryFileData) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(int, -1);
+    }
+
+    RECOMP_RETURN(bool, writeEntryDataToRecompBuffer(rdram, ctx, entry->modelData, entry->fileSize));
+}
+
+RECOMP_DLL_FUNC(PMMZobj_createDirectory) {
+    RECOMP_RETURN(bool, fs::create_directories(RECOMP_ARG_STR(0)));
+}
+
+RECOMP_DLL_FUNC(PMMZobj_isDirectoryExist) {
+    RECOMP_RETURN(bool, fs::is_directory(RECOMP_ARG_STR(0)));
+}
+
+RECOMP_DLL_FUNC(PMMZobj_readEntryU8) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    int offset = RECOMP_ARG(int, 1);
+
+    if (offset >= entry->fileSize) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    PTR(unsigned char) out = RECOMP_ARG(PTR(unsigned char), 2);
+
+    if (!out) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    MEM_BU(static_cast<size_t>(out), 0) = entry->modelData[offset];
+
+    RECOMP_RETURN(bool, true);
+}
+
+RECOMP_DLL_FUNC(PMMZobj_readEntryU16) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    int offset = RECOMP_ARG(int, 1);
+
+    if (offset >= entry->fileSize) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    PTR(unsigned char)
+    out = RECOMP_ARG(PTR(unsigned char), 2);
+
+    if (!out) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    MEM_HU(static_cast<size_t>(out), 0) = (uint16_t)(entry->modelData[offset] << 8) | entry->modelData[offset + 1];
+
+    RECOMP_RETURN(bool, true);
+}
+
+RECOMP_DLL_FUNC(PMMZobj_readEntryU32) {
+    ModelDiskEntry *entry = getDiskEntry(RECOMP_ARG(int, 0));
+
+    if (!entry) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    int offset = RECOMP_ARG(int, 1);
+
+    if (offset >= entry->fileSize) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    PTR(unsigned char)
+    out = RECOMP_ARG(PTR(unsigned char), 2);
+
+    if (!out) {
+        RECOMP_RETURN(bool, false);
+    }
+
+    MEM_HU(static_cast<size_t>(out), 0) = (uint32_t)(entry->modelData[offset] << 8) | (uint32_t)(entry->modelData[offset] << 8) | entry->modelData[offset];
+
+    RECOMP_RETURN(bool, true);
 }
---
+++ src/mod/main.c
@@ -0,0 +1,130 @@
+#include "modding.h"
+#include "global.h"
+#include "recomputils.h"
+#include "recompconfig.h"
+#include "yazmtcorelib_api.h"
+#include "playermodelmanager_api.h"
+#include "libc/string.h"
+
+RECOMP_IMPORT(".", int PMMZobj_scanForDiskEntries(const char *str));
+RECOMP_IMPORT(".", int PMMZobj_getNumDiskEntries());
+RECOMP_IMPORT(".", int PMMZobj_entryInternalNameLength(int i));
+RECOMP_IMPORT(".", int PMMZobj_entryDisplayNameLength(int i));
+RECOMP_IMPORT(".", int PMMZobj_entryAuthorNameLength(int i));
+RECOMP_IMPORT(".", bool PMMZobj_writeInternalNameToBuffer(int i, char *buffer, int bufferSize));
+RECOMP_IMPORT(".", bool PMMZobj_writeDisplayNameToBuffer(int i, char *buffer, int bufferSize));
+RECOMP_IMPORT(".", bool PMMZobj_writeAuthorNameToBuffer(int i, char *buffer, int bufferSize));
+RECOMP_IMPORT(".", int PMMZobj_getEntryFileSize(int i));
+RECOMP_IMPORT(".", bool PMMZobj_getEntryFileData(int i, char *buffer, int bufferSize));
+RECOMP_IMPORT(".", bool PMMZobj_createDirectory(char *path));
+RECOMP_IMPORT(".", bool PMMZobj_isDirectoryExist(char *path));
+
+typedef struct {
+    Gfx displayLists[PMM_DL_MAX];
+    char *internalName;
+    char *displayName;
+    char *authorName;
+    void *buffer;
+} FormModel;
+
+static FormModel *sFormModels;
+
+#define MAIN_DIR "playermodelmanager"
+#define MODEL_DIR MAIN_DIR "/models"
+
+typedef struct {
+    char *str;
+    size_t length;
+} StringInfo;
+
+// Combine, at most, 32 path strings
+char *getCombinedPath(int count, ...) {
+    const MAX_COUNT = 32;
+
+    if (count > MAX_COUNT) {
+        return NULL;
+    }
+
+    char *combinedPath = NULL;
+
+    size_t finalPathLen = 0;
+
+    StringInfo *pathStrings[count];
+
+    va_list args;
+    va_start(args, count);
+
+    for (int i = 0; i < count; ++i) {
+        StringInfo *strInfo = &pathStrings[i];
+        strInfo->str = NULL;
+        strInfo->length = 0;
+
+        strInfo->str = va_arg(args, char *);
+
+        strInfo->length = strlen(strInfo->str);
+
+        finalPathLen += strInfo->length;
+    }
+
+    va_end(args);
+
+    // make room for dir seperators
+    finalPathLen += count - 1;
+
+    // room for null byte
+    ++finalPathLen;
+
+    combinedPath = recomp_alloc(finalPathLen * sizeof(char));
+    char *pos = combinedPath;
+
+    for (int i = 0; i < count; ++i) {
+        StringInfo *strInfo = &pathStrings[i];
+
+        for (int j = 0; j < strInfo->length; ++j) {
+            *pos = strInfo->str[j];
+            ++pos;
+        }
+
+        // add directory seperators between path names
+        if (i < count - 1) {
+            // Windows/Mac/Linux all support forward slash dir seperator
+            // so there's no need to handle back slash
+            pos[0] = '/';
+            ++pos;
+        }
+    }
+
+    combinedPath[finalPathLen - 1] = '\0';
+
+    return combinedPath;
+}
+
+PLAYERMODELMANAGER_CALLBACK_REGISTER_MODELS void registerDiskModels() {
+    char *modDir = (char *)recomp_get_mod_folder_path();
+
+    char *fullModelDir = getCombinedPath(2, modDir, MODEL_DIR);
+
+    recomp_free(modDir);
+
+    PMMZobj_createDirectory(fullModelDir);
+
+    if (!PMMZobj_isDirectoryExist(fullModelDir)) {
+        recomp_printf("PlayerModelManager_ZobjSupport: Could not find %s directory", fullModelDir);
+        recomp_free(fullModelDir);
+        return;
+    }
+
+    int numDiskEntries = PMMZobj_scanForDiskEntries(fullModelDir);
+
+    for (int i = 0; i < numDiskEntries; ++i) {
+        int internalLen = PMMZobj_entryInternalNameLength(i);
+
+        if (internalLen < 1) {
+            continue;
+        }
+
+
+
+
+    }
+}
