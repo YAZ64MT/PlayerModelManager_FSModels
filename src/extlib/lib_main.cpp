245f55b12e6351698bec0093d39200bc376a69a5
Neirn <neirny@gmail.com>
2025-07-16 09:44:50 -0700

    fixed dmadata location for OOT NTSC 1.2

--- src/extlib/lib_main.cpp
+++ src/extlib/lib_main.cpp
@@ -463,10 +463,10 @@ RECOMP_DLL_FUNC(PMMZobj_clearDiskEntries) {
 static const std::unordered_map<std::string, std::pair<std::string, unsigned>> DMA_DATA_OFFSETS = {
     {"ad69c91157f6705e8ab06c79fe08aad47bb57ba7", {"OOT NTSC 1.0 (US)", 0x7430}}, // NTSC 1.0 (US)
     {"d3ecb253776cd847a5aa63d859d8c89a2f37b364", {"OOT NTSC 1.1 (US)", 0x7430}}, // NTSC 1.1 (US)
-    {"41b3bdc48d98c48529219919015a1af22f5057c2", {"OOT NTSC 1.2 (US)", 0x7430}}, // NTSC 1.2 (US)
+    {"41b3bdc48d98c48529219919015a1af22f5057c2", {"OOT NTSC 1.2 (US)", 0x7960}}, // NTSC 1.2 (US)
     {"c892bbda3993e66bd0d56a10ecd30b1ee612210f", {"OOT NTSC 1.0 (JP)", 0x7430}}, // NTSC 1.0 (JP)
     {"dbfc81f655187dc6fefd93fa6798face770d579d", {"OOT NTSC 1.1 (JP)", 0x7430}}, // NTSC 1.1 (JP)
-    {"fa5f5942b27480d60243c2d52c0e93e26b9e6b86", {"OOT NTSC 1.2 (JP)", 0x7430}}, // NTSC 1.2 (JP)
+    {"fa5f5942b27480d60243c2d52c0e93e26b9e6b86", {"OOT NTSC 1.2 (JP)", 0x7960}}, // NTSC 1.2 (JP)
 };
 
 const int ROM_LITTLE_ENDIAN = 0x40;
